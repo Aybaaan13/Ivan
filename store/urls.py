fdbdc51497680557dc9f557427e49d34f0cf6fad
Aybaaan13 <vnvctrn1@gmail.com>
2024-02-03 20:21:47 +0800

    initial commit

---
+++ store/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('', views.home, name="home"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+	path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
+    path('store/', views.store, name="store"),
+    path('login/', views.user_login, name='user_login'),
+    path('register/', views.register, name='register'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+]
